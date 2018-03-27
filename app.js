f1d4d830541697b3e614c542d774c9cc286cd02c
madalincm <madalin.cotetiu@softvision.ro>
2018-03-27 12:22:28 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-var jwt = require('jsonwebtoken');
 var newman = require('newman');
+const token = require('./token.js');
 
-var issuedAt = Math.floor(Date.now() / 1000);
-var payload = {
-  iss: 'user:11673879:778',
-  jti: Math.random().toString(),
-  iat: issuedAt,
-  exp: issuedAt + 60,
-};
-
-var secret = '140598b1138dadb26d7845f8041cbcfd67559a12c956e2cf4219dea2683ba806';  // store this securely.
-var token = jwt.sign(payload, secret, {
-  algorithm: 'HS256',  // HMAC-SHA256 signing algorithm
-});
+var generatedToken = token.generateToken();
+console.log("token from app.js", generatedToken);
 
 newman.run({
   collection: require('./addons.allizom.org.postman_collection'),
@@ -22,7 +12,7 @@ newman.run({
     "values": [
       {
         "key": "MOZILLA_TOKEN",
-        "value": "JWT " + token,
+        "value": "JWT " + generatedToken,
         "description": "",
         "type": "text",
         "enabled": true
