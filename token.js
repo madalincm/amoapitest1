4a21bab19f3a47dc563b49e5f9e3252bada658db
madalincm <madalin.cotetiu@softvision.ro>
2018-03-27 14:35:28 +0300

    refactoring

--- token.js
+++ token.js
@@ -1,14 +1,16 @@
 var jwt = require('jsonwebtoken');
+const users = require('./users.js');
+console.log(users.users[0].iss);
 
 var generateToken = () => {
     var issuedAt = Math.floor(Date.now() / 1000);
     var payload = {
-        iss: 'user:11673879:778',
+        iss: users.users[0].iss,
         jti: Math.random().toString(),
         iat: issuedAt,
         exp: issuedAt + 60,
     };
-    var secret = '140598b1138dadb26d7845f8041cbcfd67559a12c956e2cf4219dea2683ba806';  // store this securely.
+    var secret = users.users[0].secret;  // store this securely.
     var token = jwt.sign(payload, secret, {
         algorithm: 'HS256',  // HMAC-SHA256 signing algorithm
     });
---
+++ users.js
@@ -0,0 +1,17 @@
+var users = [
+    {
+        //admin user
+        "iss" : "user:11673879:778",
+        "secret": "140598b1138dadb26d7845f8041cbcfd67559a12c956e2cf4219dea2683ba806"
+    },
+    {
+        //regular user
+        "iss" : "user:11673349:990",
+        "secret": "eedf049f6ad683a717eda57fed71be59c5c718c074b5a3be4e33be9a092927de"
+    }
+]
+
+module.exports = {
+    users
+}
+
