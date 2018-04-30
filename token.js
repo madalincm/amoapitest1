64e2a067f1e21653675e94dfbc8b11895e4f81a7
madalincm <madalin.cotetiu@softvision.ro>
2018-04-30 18:27:47 +0300

    test

--- token.js
+++ token.js
@@ -4,15 +4,17 @@ const users = require('./users.js');
 var generateToken = () => {
     var issuedAt = Math.floor(Date.now() / 1000);
     var payload = {
-        iss: users.users.admin.iss,
+        iss: users.users.dev.iss,
         jti: Math.random().toString(),
         iat: issuedAt,
         exp: issuedAt + 60,
     };
-    var secret = users.users.admin.secret;  // store this securely.
+    var secret = users.users.dev.secret;  // store this securely.
     var token = jwt.sign(payload, secret, {
         algorithm: 'HS256',  // HMAC-SHA256 signing algorithm
     });
+
+console.log(token);
 return token;
 }
 
